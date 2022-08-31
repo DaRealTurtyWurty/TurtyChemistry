beb1d8a23d571e2e22b91de83a52dd45872634d5
xana43 <jaylen@woontech.com>
2022-08-31 14:20:50 -0400

    added a helper method in block entity init to help with block entity registration

--- src/main/java/io/github/darealturtywurty/turtychemistry/core/init/BlockEntityInit.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/core/init/BlockEntityInit.java
@@ -3,6 +3,8 @@ package io.github.darealturtywurty.turtychemistry.core.init;
 import io.github.darealturtywurty.turtychemistry.TurtyChemistry;
 import io.github.darealturtywurty.turtychemistry.common.block.entity.ClayAlloyFurnaceBlockEntity;
 import io.github.darealturtywurty.turtylib.core.init.AbstractInit;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +14,12 @@ public final class BlockEntityInit extends AbstractInit {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister
         .create(ForgeRegistries.BLOCK_ENTITY_TYPES, TurtyChemistry.MODID);
 
-    public static final RegistryObject<BlockEntityType<ClayAlloyFurnaceBlockEntity>> CLAY_ALLOY_FURNACE = BLOCK_ENTITIES.register("clay_alloy_furnace",
-            () -> BlockEntityType.Builder.of(ClayAlloyFurnaceBlockEntity::new, BlockInit.CLAY_ALLOY_FURNACE.get()).build(null));
+    public static final RegistryObject<BlockEntityType<ClayAlloyFurnaceBlockEntity>> CLAY_ALLOY_FURNACE = registerNewBlockEntity("clay_allow_furnace",ClayAlloyFurnaceBlockEntity::new,BlockInit.CLAY_ALLOY_FURNACE.get());
+
+
+
+    private static <T extends BlockEntity>RegistryObject<BlockEntityType<T>> registerNewBlockEntity(final String name, final BlockEntityType.BlockEntitySupplier<T> blockEntitySupplier, final Block block)
+    {
+        return BLOCK_ENTITIES.register(name,() -> BlockEntityType.Builder.of(blockEntitySupplier,block).build(null));
+    }
 }
