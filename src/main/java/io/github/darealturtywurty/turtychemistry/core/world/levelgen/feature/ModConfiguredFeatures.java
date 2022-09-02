d2d0ac21d2c40f3cedf93a8a492a6da394b16246
rc8toast <77547837+rc8toast@users.noreply.github.com>
2022-09-02 21:55:54 +0200

    Update src/main/java/io/github/darealturtywurty/turtychemistry/core/world/levelgen/feature/ModConfiguredFeatures.java
    Co-authored-by: DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>

--- src/main/java/io/github/darealturtywurty/turtychemistry/core/world/levelgen/feature/ModConfiguredFeatures.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/core/world/levelgen/feature/ModConfiguredFeatures.java
@@ -16,7 +16,7 @@ import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlac
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
 public class ModConfiguredFeatures {
-    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> RUBBER_TREE = register("rubber_tree", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.OAK_LOG), new RubberTreeTrunkPlacer(5, 2, 1, ConstantInt.of(5), UniformInt.of(1, 2)), BlockStateProvider.simple(Blocks.OAK_LEAVES), new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 2), new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
+    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> RUBBER_TREE = register("rubber_tree", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(BlockInit.RUBBER_TREE_BLOCK.get()), new RubberTreeTrunkPlacer(5, 2, 1, ConstantInt.of(5), UniformInt.of(1, 2)), BlockStateProvider.simple(BlockInit.RUBBER_TREE_LEAVES.get()), new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 2), new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
 
     private static <FC extends FeatureConfiguration, F extends Feature<FC>> Holder<ConfiguredFeature<FC, ?>> register(String name, F feature, FC featureConfiguration) {
         return FeatureUtils.register(TurtyChemistry.MODID + ":" + name, feature, featureConfiguration);
