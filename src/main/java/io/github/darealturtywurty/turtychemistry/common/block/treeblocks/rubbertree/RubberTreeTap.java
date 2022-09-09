dc839a3fe3b72602b5d7f208e3de7991e86c7ac3
xana43 <jaylen@woontech.com>
2022-09-08 22:04:56 -0400

    made the rubber treetap drop items on the ground if it's destroyed by anything that's not the player

--- src/main/java/io/github/darealturtywurty/turtychemistry/common/block/treeblocks/rubbertree/RubberTreeTap.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/common/block/treeblocks/rubbertree/RubberTreeTap.java
@@ -9,6 +9,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -60,6 +61,15 @@ public final class RubberTreeTap extends Block {
             playerInventory.setItem(playerInventory.getFreeSlot(), new ItemStack(ItemInit.LATEX.get()));
         }
         return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+    }
+
+    @Override
+    public void onRemove(final @NotNull BlockState state, final @NotNull Level level, final @NotNull BlockPos pos, final @NotNull BlockState newState, final boolean isMoving) {
+        final int currentLatexAmount = state.getValue(LATEX_AMOUNT);
+        for (int i = 0; i < currentLatexAmount; i++) {
+            level.addFreshEntity(new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(ItemInit.LATEX.get())));
+        }
+        super.onRemove(state, level, pos, newState, isMoving);
     }
 
     @Override
