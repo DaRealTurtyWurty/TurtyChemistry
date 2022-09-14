2f2d3a2f1ee79721480dabf2a6e5cbcfa7c49961
xana43 <jaylen@woontech.com>
2022-09-14 00:43:33 -0400

    added a recipe to create carbon with any coal and the hammer

--- src/main/java/io/github/darealturtywurty/turtychemistry/common/block/entity/AnvilBlockEntity.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/common/block/entity/AnvilBlockEntity.java
@@ -15,7 +15,6 @@ public final class AnvilBlockEntity extends ModularBlockEntity {
     public AnvilBlockEntity(final BlockPos pos, final BlockState state) {
         super(BlockEntityInit.ANVIL.get(), pos, state);
         this.inventoryModule = this.addModule(new InventoryModule(this, 1));
-
     }
 
     public void setStackInSlot(final ItemStack stack) {
--- src/main/java/io/github/darealturtywurty/turtychemistry/common/item/BasicHammer.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/common/item/BasicHammer.java
@@ -1,7 +1,11 @@
 package io.github.darealturtywurty.turtychemistry.common.item;
 
 import io.github.darealturtywurty.turtychemistry.common.block.entity.AnvilBlockEntity;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,11 +15,46 @@ public final class BasicHammer extends ChemistryItem {
     }
 
     @Override
+    public boolean isDamageable(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getMaxDamage(ItemStack stack) {
+        return 70;
+    }
+
+    @Override
+    public ItemStack getCraftingRemainingItem(ItemStack itemStack) {
+        final ItemStack stack = new ItemStack(this);
+        stack.setDamageValue(itemStack.getDamageValue() + 1);
+        return stack;
+    }
+
+    @Override
+    public int getMaxStackSize(ItemStack stack) {
+        return 1;
+    }
+
+    @Override
+    public boolean hasCraftingRemainingItem(ItemStack stack) {
+        return stack.getDamageValue() < stack.getMaxDamage() - 1;
+    }
+
+    @Override
     public @NotNull InteractionResult useOn(UseOnContext pContext) {
-        if (pContext.getLevel().getBlockEntity(pContext.getClickedPos()) instanceof
-                AnvilBlockEntity alternateAnvil && pContext.getPlayer().isCrouching()) {
-            alternateAnvil.smithItem();
+        final Player player = pContext.getPlayer();
+        if (pContext.getLevel().getBlockEntity(
+                pContext.getClickedPos()) instanceof AnvilBlockEntity anvilBlockEntity && player.isCrouching()) {
+            anvilBlockEntity.smithItem();
+            player.level.playSound(null, pContext.getClickedPos(), SoundEvents.ANVIL_USE, SoundSource.BLOCKS, 1, 1);
+            if (player.getUseItem().getDamageValue() < player.getUseItem().getMaxDamage()) {
+                player.getUseItem().setDamageValue(player.getUseItem().getDamageValue() + 1);
+            } else {
+                player.getUseItem().shrink(1);
+            }
+            return InteractionResult.CONSUME;
         }
-        return super.useOn(pContext);
+        return InteractionResult.PASS;
     }
 }
