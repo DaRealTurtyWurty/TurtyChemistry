7ff763c58c28534102ce20f6ce90a7b953216555
xana43 <jaylen@woontech.com>
2022-09-14 02:24:43 -0400

    matched all implementation of item class to prevent some weird issues with obfuscation mapping and the actual mapping being different and unable to be found after a few runs

--- src/main/java/io/github/darealturtywurty/turtychemistry/mixin/ItemMixin.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/mixin/ItemMixin.java
@@ -5,12 +5,13 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.LevelReader;
 import net.minecraftforge.common.extensions.IForgeItem;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Item.class)
-public abstract class ItemMixin implements IForgeItem {
+public abstract class ItemMixin implements ItemLike, IForgeItem {
 
     @Override
     public boolean doesSneakBypassUse(ItemStack stack, LevelReader level, BlockPos pos, Player player) {
