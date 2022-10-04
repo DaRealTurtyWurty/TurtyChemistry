950c3ebf945ba40bb8b348c299a39ada48efa228
xana43 <jaylen@woontech.com>
2022-10-04 15:26:52 -0400

    fixed ingots that have a temperature on them not rendering when in the hand

--- src/main/java/io/github/darealturtywurty/turtychemistry/mixins/ItemMixin.java
+++ src/main/java/io/github/darealturtywurty/turtychemistry/mixins/ItemMixin.java
@@ -4,11 +4,13 @@ import io.github.darealturtywurty.turtychemistry.common.block.entity.AnvilBlockE
 import io.github.darealturtywurty.turtychemistry.core.MixinHooks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -30,7 +32,6 @@ public abstract class ItemMixin implements ItemLike, IForgeItem {
         }
         return IForgeItem.super.doesSneakBypassUse(stack, level, pos, player);
     }
-
     @Inject(method = "appendHoverText", at = @At("HEAD"))
     public void turtychemistry$appendHoverText(final ItemStack pStack, final Level pLevel, final List<Component> pTooltipComponents, final TooltipFlag pIsAdvanced, final CallbackInfo ci) {
         MixinHooks.ItemMixinImplementation.addTemperatureToolTip(pStack, pTooltipComponents);
@@ -38,7 +39,7 @@ public abstract class ItemMixin implements ItemLike, IForgeItem {
 
     @Inject(method = "inventoryTick", at = @At("HEAD"))
     public void turtychemistry$inventoryTick(final ItemStack pStack, final Level pLevel, final Entity pEntity, final int pSlotId, final boolean pIsSelected, final CallbackInfo ci) {
-        MixinHooks.ItemMixinImplementation.calculateStackTemperature(pStack, pLevel, pEntity);
+            MixinHooks.ItemMixinImplementation.calculateStackTemperature(pStack, pLevel, pEntity);
     }
 
 
